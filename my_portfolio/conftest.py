ffa5bdaa66017abc6c2622b36721509af6876505
zarrai saif edine <zarraisaifedine@gmail.com>
2022-07-09 02:12:58 +0200

    init

---
+++ my_portfolio/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from my_portfolio.users.models import User
+from my_portfolio.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
