bd04b90a975fd001d4a0ee051a95054e028fe5f7
zarrai saif edine <zarraisaifedine@gmail.com>
2022-08-17 06:50:04 +0200

    pass actions

--- my_portfolio/users/tests/test_swagger.py
+++ my_portfolio/users/tests/test_swagger.py
@@ -5,17 +5,15 @@ pytestmark = pytest.mark.django_db
 
 
 def test_swagger_accessible_by_admin(admin_client):
-    pass
-    # url = reverse("api-docs")
-    # response = admin_client.get(url)
-    # assert response.status_code == 200
+    url = reverse("api-docs")
+    response = admin_client.get(url)
+    assert response.status_code == 200
 
 
 def test_swagger_ui_not_accessible_by_normal_user(client):
-    pass
-    # url = reverse("api-docs")
-    # response = client.get(url)
-    # assert response.status_code == 403
+    url = reverse("api-docs")
+    response = client.get(url)
+    assert response.status_code == 403
 
 
 def test_api_schema_generated_successfully(admin_client):
